9ad304c5d6bfcc0395766cbaf192d1d6b8ee37c5
sho088 <sho130809@gmail.com>
2026-05-05 23:16:11 +0900

    ホーム作成2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
 function Home(){
     return(
-        <div>
+        <div　style={{ padding: '20px', textAlign: 'center' }}>
             <h1>ホーム画面</h1>
             <p>ここにナレッジシステムを構築する</p>
         </div>
-    )
-}
+    );
+};
 
 export default Home
